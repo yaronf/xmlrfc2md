9f6197f56bb58f458f8637a323bc07c381d67cd5
ysheffer <Yaron_Sheffer@intuit.com>
2023-12-05 00:26:20 +0200

    Most text extracted

--- xml2md.py
+++ xml2md.py
@@ -1,6 +1,17 @@
 import xml.etree.ElementTree as ET
-import yaml  # pyyaml package
+# import yaml  # pyyaml package
 import sys
+
+
+def collapse_spaces(t):
+    lines = t.splitlines()
+    out = []
+    for ln in lines:
+        lst = ln.lstrip()
+        if lst != ln:
+            lst = " " + lst
+        out.append(lst)
+    return "\n".join(out)
 
 
 def extract_text(root):
@@ -16,24 +27,54 @@ def section_title(elem: ET, level):
     if name is None:
         print("Section with no name")
         return ""
-    return "#"*level + " " + name.text + " " + anchor + "\n"
+    return "#" * level + " " + name.text + " " + anchor + "\n"
 
 
-def extract_sections(root, level):
+def extract_xref(elem):
+    target = elem.get("target")
+    section = elem.get("section")
+    section_format = elem.get("sectionFormat")
+
+    if target is None:
+        print("Missing target in xref")
+        return "badxref"
+    if target.startswith("RFC"):
+        if section_format != "of" and section_format != "comma":
+            print("Unsupported xref section format: " + section_format)
+            return "badxref"
+        if section is None:
+            return "{{" + target + "}}"
+        else:
+            if section_format == "of":
+                return "Section " + section + " of {{" + target + "}}"
+            else:  # comma
+                return "{{" + target + "}}, Section " + section
+    return "{{" + target + "}}"
+
+
+def extract_sections(root, level, bulleted):
     """Extract text from a sequence of <t> elements, possibly nested
 """
     output = ""
     if root.text is not None:
-        output += root.text.lstrip()
+        output += collapse_spaces(root.text)
     for elem in root:
         match elem.tag:
             case "t":
-                output += extract_sections(elem, level)
-                output += "\n\n"
+                output += extract_sections(elem, level, False)
+                output += "\n"
+            case "li":
+                pre = "* " if bulleted else ""
+                output += pre + extract_sections(elem, level, False)
+                output += "\n"
             case "section":
                 output += section_title(elem, level + 1)
-                output += extract_sections(elem, level + 1)
-                output += "\n\n"
+                output += extract_sections(elem, level + 1, False)
+                output += "\n"
+            case "ul":
+                output += extract_sections(elem, level, True)
+            case "xref":
+                output += extract_xref(elem)
             case "bcp14":
                 output += elem.text
             case "name":
@@ -41,7 +82,7 @@ def extract_sections(root, level):
             case _:
                 print("Skipping unknown element: ", elem.tag)
         if elem.tail is not None:
-            output += elem.tail
+            output += collapse_spaces(elem.tail)
     return output
 
 
@@ -63,9 +104,15 @@ def parse_rfc(infile):
     middle = root.find("middle")
     if middle == "":
         sys.exit("Cannot find middle part of document")
-    t = extract_sections(middle, 0)
+    t = extract_sections(middle, 0, False)
     output += "--- middle\n\n"
     output += t
+
+    back = root.find("back")
+    if back != "":
+        t = extract_sections(back, 0, False)
+        output += "--- back\n\n"
+        output += t
 
     return output
 
